0d66ceb1c7eb7a2cb41308687816c96dddd1ef04
Seungwook Han <sh3264@columbia.edu>
2023-08-21 11:55:53 -0400

    fixed args

--- examples/generate_qmodel.py
+++ examples/generate_qmodel.py
@@ -7,14 +7,26 @@ from alpaca_farm.auto_annotations import PairwiseAutoAnnotator, alpaca_leaderboa
 
 # Set up argparse to take in the model name and checkpoint dir
 parser = argparse.ArgumentParser()
-parser.add_argument("--reward_name_or_path", type=str, default="huggyllama/llama-7b"
+parser.add_argument("--decoder_name_or_path", type=str, default="huggyllama/llama-7b"
                     , help="The name or path of the decoder to use")
-parser.add_argument('--reward_checkpoint_dir', type=str, default=''
+parser.add_argument('--decoder_checkpoint_dir', type=str, default=''
                     , help="The path to the checkpoint directory of the decoder (adapter weigthts)")
-parser.add_argument('--output1_path', type=str, default=''
+parser.add_argument('--q_checkpoint_dir', type=str, default=''
                     , help="The path to the checkpoint directory of the q model (adapter weights)")
-parser.add_argument('--output2_path', type=str, default='./output.json'
+parser.add_argument('--path_to_data', type=str, default='./output.json'
                     , help='The path to the output json file')
+parser.add_argument('--num_return_sequences', type=int, default=1
+                    , help='The number of sequences to return from the decoder')
+parser.add_argument('--temp', type=float, default=0.7
+                    , help='The temperature to use for decoding')
+parser.add_argument('--per_device_batch_size', type=int, default=12
+                    , help='The batch size to use for decoding')
+parser.add_argument('--load_in_4_bits', type=bool, default=True
+                    , help='Whether to load the model in 4 bits')
+parser.add_argument('--beta', type=float, default=1.0
+                    , help='The beta value to use for weighting the q model')
+parser.add_argument('--exp_name', type=str, default='qmodel'
+                    , help='The name of the experiment')
 args = parser.parse_args()
 
 if 'OPENAI_API_KEY' not in os.environ:
