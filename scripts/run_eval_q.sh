30e4fe65ec5d8894fbeb885c199c9926d172a22e
Seungwook Han <swhan@mit.edu>
2023-08-22 13:41:20 -0400

    scripts for running generate

---
+++ scripts/run_eval_q.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CUDA_VISIBLE_DEVICES=0 python examples/evaluate_qmodel.py \
+  --decoder_name_or_path huggyllama/llama-7b \
+  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/test_5/ \
+  --q_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla_1/ \
+  --load_in_4_bits True \
+  --temp 0.7 \
+  --per_device_batch_size 12 \
+  --path_to_data ./outputs_q.json \
+  --beta 1.0 
--- scripts/run_eval_sft.sh
+++ scripts/run_eval_sft.sh
@@ -3,7 +3,7 @@
 CUDA_VISIBLE_DEVICES=0 python examples/evaluate_qmodel.py \
   --decoder_name_or_path huggyllama/llama-7b \
   --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/test_5/ \
-  --q_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla_1/ \
-  --path_to_data ./output.json \
-  --beta 1.0 \
-  --exp_name qmodel_eval
+  --load_in_4_bits True \
+  --temp 0.7 \
+  --per_device_batch_size 12 \
+  --path_to_data ./outputs_sft.json
