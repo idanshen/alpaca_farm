a39891697ef7771f29abae55d306265eca05e144
Seungwook Han <swhan@mit.edu>
2023-08-22 13:02:17 -0400

    finished implementing rm eval code

--- examples/evaluate_outputs_rm.py
+++ examples/evaluate_outputs_rm.py
@@ -5,12 +5,42 @@ from typing import List, Dict, Any
 import transformers
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from dataclasses import dataclass, field
 
 from best_of_n import run_decode_augmented
 from alpaca_farm import data_utils, common
 from alpaca_farm.utils import jload, jdump
 from alpaca_farm.models import reward_model as reward_model_module
 from alpaca_farm.rl.ppo_trainer import _make_left_padded_tokenizer
+
+"""
+Arguments for the script
+"""
+@dataclass
+class Arguments:
+    reward_model_name_or_path: str = field(
+        default="huggyllama/llama-7b", metadata={"help": "Name to a huggingface native pretrained model or path to a model on disk."}
+    ),
+    reward_model_checkpoint_dir: str = field(
+        default="./", metadata={"help": "Path to a checkpoint directory."}
+    ),
+    output_filepath: str = field(
+        default="./outputs.json", metadata={"help": "Path to a checkpoint directory."}
+    ),
+    path_to_result: str = field(
+        default="./results.json", metadata={"help": "The path to the output json file."}
+    ),
+    per_device_batch_size: int = field(
+        default=12, metadata={"help": "The path to the output json file."}
+    ),
+    load_in_4_bits: bool = field(
+        default=True, metadata={"help": "Whether to load the model in 4 bits."}
+    ),
+    four_bits: bool = field(default=True, metadata={"help": "If True, uses 4-bit quantization."})
+    bfloat16: bool = field(
+        default=False, metadata={"help": "If True, uses bfloat16 quantization. If lora and four_bits are True, bfloat16 is used for the lora weights."})
+    use_lora: bool = field(default=True, metadata={"help": "If True, uses LoRA."})
+    exp_name: str = field(default="eval_outputs_rm", metadata={"help": "The name of the experiment."}),
 
 
 def make_reward_model(args, is_trainable=False):
@@ -45,44 +75,38 @@ def evaluate_data(args, reward_model, eval_data_list_dict) -> List[Dict[str, Any
     eval_data_list_dict: List[Dict[str, Any]], the generated data to evaluate
     """
 
-    generated_data = []
     pbar = tqdm(total=len(eval_data_list_dict), desc="eval")
+    rewards_list = []
 
+    print('Evaluating reward scores...')
     for i, idx in enumerate(range(len(eval_data_list_dict)), step=args.per_device_batch_size):
-        batch_list_dict = eval_data_list_dict[idx:idx+args.per_device_batch_size]
-        
-        batch_full_outputs = ...
+        if len(eval_data_list_dict) < (idx + args.per_device_batch_size):
+            batch_list_dict = eval_data_list_dict[idx:idx+args.per_device_batch_size]
+        else:
+            batch_list_dict = eval_data_list_dict[idx:]
+
+        batch_full_outputs = [l['prompt'] + ' ' + l['output'] for l in batch_list_dict]
         encoded_full_responses = reward_tokenizer(batch_full_outputs, return_tensors="pt", padding=True, truncation=True)
         encoded_full_responses, = common.prepare_inputs((encoded_full_responses, ), device=0)
         reward_outputs = reward_model(**encoded_full_responses)
-        rewards = reward_outputs['rewards'].cpu().detach().numpy()
-
+        rewards = reward_outputs['rewards'].squeeze().cpu().detach().numpy()
+        
+        rewards_list.extend(list(rewards))
         # join data
-        pbar.update(args.per_device_batch_size)
+        pbar.update(len(batch_list_dict))
+    
+    print('Combining reward outputs into outputs...')
+    
+    for i in range(len(eval_data_list_dict)):
+        eval_data_list_dict[i]['reward'] = rewards_list[i]
 
-        # TODO: maybe do everything here the batching and tokenizing? and joining the output files too actually
+    return eval_data_list_dict
 
-        return generated_data
 
 if __name__ == "__main__":
-    # Set up argparse to take in the model name and checkpoint dir
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--reward_model_name_or_path", type=str, default="huggyllama/llama-7b"
-                        , help="The name or path of the decoder to use")
-    parser.add_argument('--reward_checkpoint_dir', type=str, default=''
-                        , help="The path to the checkpoint directory of the decoder (adapter weigthts)")
-    parser.add_argument('--output_filepath', type=str, default='./outputs.json',
-                        help='The path to the output json file to evaluate the samples of')
-    parser.add_argument('--path_to_result', type=str, default='./results.json'
-                        , help='The path to the output json file')
-    parser.add_argument('--per_device_batch_size', type=int, default=12
-                        , help='The batch size to use for decoding')
-    parser.add_argument('--load_in_4_bits', type=bool, default=True
-                        , help='Whether to load the model in 4 bits')
-    parser.add_argument('--exp_name', type=str, default='eval_outputs_rm'
-                        , help='The name of the experiment')
-    args = parser.parse_args()
-
+    # parse arguments
+    parser = transformers.HfArgumentParser(Arguments)
+    args, = parser.parse_args_into_dataclasses()
 
     if os.path.isfile(args.output_filepath):
         eval_data_list_dict = jload(args.output_filepath)
@@ -92,18 +116,9 @@ if __name__ == "__main__":
     print('Loaded data, now evaluating reward scores...')
     reward_tokenizer: transformers.PreTrainedTokenizer = _make_left_padded_tokenizer(model_name_or_path=args.reward_model_name_or_path)
     reward_model = make_reward_model(args=args)
-    # eval_data_module: dict = data_utils.make_eval_data_module(tokenizer=reward_tokenizer)
-    # eval_datset = eval_data_module["dataset"]
-    # data_collator = eval_data_module["data_collator"]
-    # print(f"Test dataset size: {len(eval_datset)}")
-    # data_loader = DataLoader(
-    #     dataset=eval_datset,
-    #     collate_fn=data_collator,
-    #     batch_size=args.per_device_batch_size,
-    #     shuffle=True,
-    #     drop_last=True,
-    # )
+
     eval_data = evaluate_data(args, reward_model, eval_data_list_dict)
 
     # combine output file and reward outputs
-    jdump(eval_data, args.output_file)
+    print(f'Saving results to file {args.path_to_result}...')
+    jdump(eval_data, args.path_to_result)
