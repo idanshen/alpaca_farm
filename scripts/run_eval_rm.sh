7e9c31a857b194f0391b50cc37183dcaffb1dee7
Seungwook Han <sh3264@columbia.edu>
2023-08-25 13:51:05 -0400

    Fixed 4 bit arg

--- scripts/run_eval_rm.sh
+++ scripts/run_eval_rm.sh
@@ -4,6 +4,6 @@ CUDA_VISIBLE_DEVICES=0 python examples/evaluate_outputs_rm.py \
   --reward_model_name_or_path Tristan/gpt2_reward_summarization \
   --output_filepath ./outputs_sft.json \
   --path_to_result ./outputs_rewards_sft.json \
-  --load_in_4_bits True \
+  --four_bits True \
   --per_device_batch_size 12
 
