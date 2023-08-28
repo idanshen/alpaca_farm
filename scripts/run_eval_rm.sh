b62f93ae645f2be606e7a8d474daf034d43613a9
Seungwook Han <swhan@mit.edu>
2023-08-28 10:49:45 -0400

    script updates

--- scripts/run_eval_rm.sh
+++ scripts/run_eval_rm.sh
@@ -2,9 +2,9 @@
 
 CUDA_VISIBLE_DEVICES=0 python examples/evaluate_outputs_rm.py \
   --reward_model_name_or_path Tristan/gpt2_reward_summarization \
-  --output_filepath ./outputs_sft.json \
-  --path_to_result ./outputs_rewards_sft.json \
+  --output_filepath ./outputs_ppo.json \
+  --path_to_result ./outputs_rewards_ppo.json \
   --four_bits True \
-  --per_device_batch_size 1 \
-  --fp16
+  --per_device_batch_size 4
+  #--fp16
 
