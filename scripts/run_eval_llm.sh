1be5054c46b0e2a66339df61c845bb329a13b26d
Seungwook Han <swhan@mit.edu>
2023-08-28 17:37:11 -0400

    fixed arg:

--- scripts/run_eval_llm.sh
+++ scripts/run_eval_llm.sh
@@ -3,6 +3,6 @@
 python examples/evaluate_outputs_llm.py \
   --output_filepath1 ./outputs/outputs_sft.json \
   --output_filepath2 ./outputs/outputs_ppo.json \
-  --eval_outputs_llm eval_outputs_sft_vs_ppo
+  --exp_name eval_outputs_sft_vs_ppo
 # --path_to_result ./outputs_rewards_ppo.json
 
