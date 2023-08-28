912ece9999d471f1c742ffeac87b8c41a1d38e5b
Seungwook Han <swhan@mit.edu>
2023-08-28 17:35:15 -0400

    fixed import

--- src/alpaca_farm/auto_annotations/__init__.py
+++ src/alpaca_farm/auto_annotations/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .eval import PairwiseAutoAnnotator, alpaca_leaderboard
+from .eval import PairwiseAutoAnnotator, alpaca_leaderboard, alpaca_leaderboard_general
