19339122806fe1836019eac6e6baa8c77254a45d
Seungwook Han <swhan@mit.edu>
2023-08-30 13:53:51 -0400

    updates on scripts

--- scripts/run_eval_llm.sh
+++ scripts/run_eval_llm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python examples/evaluate_outputs_llm.py \
-  --output_filepath1 ./outputs/outputs_sft.json \
+  --output_filepath1 ./outputs/outputs_fqecql2_beta2.0.json \
   --output_filepath2 ./outputs/outputs_ppo.json \
-  --exp_name eval_outputs_sft_vs_ppo
+  --exp_name eval_outputs_ppo_vs_fqecql2beta2.0
 # --path_to_result ./outputs_rewards_ppo.json
 
