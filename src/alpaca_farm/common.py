58e6bf3c34f2b382207214d41bb429265418337f
Seungwook Han <swhan@mit.edu>
2023-08-30 13:53:39 -0400

    fixed import

--- src/alpaca_farm/common.py
+++ src/alpaca_farm/common.py
@@ -20,7 +20,7 @@ import types
 import warnings
 from pathlib import Path
 from typing import Any, Callable, Dict, Mapping, Optional, Sequence, Union
-from optimum.bettertransformer import BetterTransformer
+#from optimum.bettertransformer import BetterTransformer
 
 import accelerate
 import torch
