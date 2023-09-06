cca5b3956c819d6f4887bfca6562c794c4bde238
Seungwook Han <swhan@mit.edu>
2023-09-06 13:19:49 -0400

    updated scripts with latest flags

--- scripts/run_gen_ppo.sh
+++ scripts/run_gen_ppo.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=1 python examples/generate_qmodel.py \
+CUDA_VISIBLE_DEVICES=6 python examples/generate_qmodel.py \
   --decoder_name_or_path huggyllama/llama-7b \
-  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla4/ \
+  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla_tristan/ \
   --load_in_4_bits True \
   --temp 0.7 \
-  --per_device_batch_size 4 \
-  --path_to_data ./outputs_ppo.json \
+  --per_device_batch_size 1 \
+  --path_to_result ./outputs_ppo_tristan.json \
   --dataset_path argilla/news-summary \
-  --dataset_name comparisons
+  --dataset_name comparisons \
+  --fp16
--- scripts/run_ppo.sh
+++ scripts/run_ppo.sh
@@ -4,11 +4,13 @@ export WANDB_DATA_DIR=/data/pulkitag/models/idanshen/shared/wandb/
 export WANDB_CACHE_DIR=/data/pulkitag/models/idanshen/shared/wandb/wandb_cache/
 
 bash examples/scripts/rlhf_ppo.sh \
-  /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla4/ \
+  /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla2/ \
   run_ppo_news_summarization_argilla \
   Tristan/gpt2_reward_summarization \
   huggyllama/llama-7b \
   /data/pulkitag/models/idanshen/shared/models/sft/test_5/ \
-  0.0067 \
+  0.05 \
   argilla/news-summary \
   comparisons
+
+#Tristan/gpt2_reward_summarization \
