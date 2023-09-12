fixed kl impl

src/alpaca_farm/rl/ppo_trainer.py
@@ -284,23 +284,15 @@ class PPOTrainer(rl_trainer.RLTrainer):
 
         entropy = outputs["entropies"].mean()
         approxkl = 0.5 * ((logprob - old_logprob) ** 2.0).mean()
-        realkl = self.kl(torch.softmax(old_logprob / self.args.temperature, dim=-1), torch.softmax(logprob / self.args.temperature, dim=-1))
+        realkl = self.kl(torch.softmax(old_logprob.detach() / self.args.temperature, dim=-1), torch.softmax(logprob.detach() / self.args.temperature, dim=-1))
         realkl = torch.clamp(realkl, min=0.0, max=100.0).mean()
-
-        if self.record_kl:
-            kl = self.kl(torch.softmax(scores/self.temperature, dim=-1), torch.softmax(augmented_q_outputs/self.temperature, dim=-1))
-            kl = torch.clamp(kl, min=0.0, max=100.0)
-            self.average_kl = (self.average_kl * self.num_points + kl) / (self.num_points + 1)
-            self.num_points += 1
-        return augmented_q_outputs
-
 
         return_mean, return_var = returns.mean(), returns.var(unbiased=False)
         value_mean, value_var = values.mean(), values.var(unbiased=False)
 
         stats = dict(
             loss=dict(policy=pg_loss, value=vf_loss, total=loss),
-            policy=dict(entropy=entropy, approxkl=approxkl, clipfrac=pg_clipfrac),
+            policy=dict(entropy=entropy, approxkl=approxkl, realkl=realkl, clipfrac=pg_clipfrac),
             returns=dict(mean=return_mean, var=return_var),
             val=dict(
                 vpred=vpred.mean(),