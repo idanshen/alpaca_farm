0560b726aa103a4704305da039b3fa3c783ffc4f
Seungwook Han <swhan@mit.edu>
2023-10-05 13:54:41 -0400

    fixed args

--- scripts/run_create_dataset_sft.sh
+++ scripts/run_create_dataset_sft.sh
@@ -2,13 +2,14 @@
 
 # assign first argument to gpu variable
 gpu=$1
+output_file=$2
 
 CUDA_VISIBLE_DEVICES=$gpu python examples/create_validation_dataset.py \
   --policy_model_name_or_path huggyllama/llama-7b \
   --policy_model_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/test_5/ \
   --four_bits True \
   --per_device_batch_size 1 \
-  --output_file ./openai_summarize_from_feedback_fqe_train.json \
+  --output_file $2 \
   --dataset_path openai/summarize_from_feedback \
   --dataset_name comparisons \
   --flash_attn True \
