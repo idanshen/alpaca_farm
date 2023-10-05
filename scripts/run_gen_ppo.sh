89ac33208f0120b7b1c828fb65907149cef64623
Seungwook Han <swhan@mit.edu>
2023-10-05 15:38:46 -0400

    added greedy and using args for inputs

--- scripts/run_gen_ppo.sh
+++ scripts/run_gen_ppo.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=6 python examples/generate_qmodel.py \
+ppo_checkpoint=$1
+result_file=$2
+gpu=$3
+
+CUDA_VISIBLE_DEVICES=$gpu python examples/generate_qmodel.py \
   --decoder_name_or_path huggyllama/llama-7b \
-  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla_tristan/ \
+  --decoder_checkpoint_dir $ppo_checkpoint \
   --load_in_4_bits True \
-  --temp 0.7 \
+  --temp 1.0 \
+  --greedy True \
   --per_device_batch_size 1 \
-  --path_to_result ./outputs_ppo_tristan.json \
+  --path_to_result $2 \
   --dataset_path argilla/news-summary \
   --dataset_name comparisons \
-  --fp16
+  --fp16 \
+  --flash_attn
