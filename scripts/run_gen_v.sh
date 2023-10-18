b9548c887b5c0cc9aee469ee2ba9634499ea2003
Seungwook Han <swhan@mit.edu>
2023-10-18 14:52:24 -0400

    fve model generation script

--- scripts/run_gen_v.sh
+++ scripts/run_gen_v.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python examples/generate_qmodel.py \
+topk=$1
+beta=$2
+gpu=$3
+
+CUDA_VISIBLE_DEVICES=$gpu python examples/generate_qmodel.py \
   --decoder_name_or_path huggyllama/llama-7b \
-  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/ppo_news_sum_argilla4/ \
-  --q_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/fve_news_sum_argilla/fve/ \
-  --topk 20 \
+  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/test_5/ \
+  --q_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/new_models_for_eval/fve_norm_10/ \
+  --topk "$topk" \
   --load_in_4_bits True \
   --temp 0.7 \
-  --per_device_batch_size 1 \
-  --path_to_result outputs_fve_beta_1.0_topk20.json \
-  --beta 1.0 \
+  --per_device_batch_size 16 \
+  --path_to_result "outputs_fve_tristan_beta$beta_topk$topk.json" \
+  --beta "$beta" \
   --dataset_path argilla/news-summary \
   --dataset_name comparisons \
   --fp16 \
