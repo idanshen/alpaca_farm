e071c2d1b44126101edfdaadca13c2a084807607
Seungwook Han <swhan@mit.edu>
2023-10-18 15:19:30 -0400

    fixed script

--- scripts/run_gen_v.sh
+++ scripts/run_gen_v.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-topk=$1
-beta=$2
-gpu=$3
+vmodel=$1
+topk=$2
+beta=$3
+gpu=$4
 
 CUDA_VISIBLE_DEVICES=$gpu python examples/generate_qmodel.py \
   --decoder_name_or_path huggyllama/llama-7b \
   --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/test_5/ \
-  --q_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/new_models_for_eval/fve_norm_10/ \
+  --v_checkpoint_dir "$vmodel" \
   --topk "$topk" \
   --load_in_4_bits True \
   --temp 0.7 \
-  --per_device_batch_size 16 \
+  --per_device_batch_size 1 \
   --path_to_result "outputs_fve_tristan_beta$beta_topk$topk.json" \
   --beta "$beta" \
   --dataset_path argilla/news-summary \
