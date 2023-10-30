5c67bd48fe94c78b213115747d1b4182ac363189
Seungwook Han <swhan@mit.edu>
2023-10-30 16:55:38 -0400

    updated params for model

--- scripts/run_ppo_seahorse.sh
+++ scripts/run_ppo_seahorse.sh
@@ -6,7 +6,7 @@ export WANDB_CACHE_DIR=/data/pulkitag/models/idanshen/shared/wandb/wandb_cache/
 bash examples/scripts/rlhf_ppo.sh \
   ./ppo_seahorse_flan_t5_kl0.0067/ \
   run_ppo_seahorse_flan_t5 \
-  ./q_five_final/ \
+  /data/pulkitag/models/idanshen/shared/models/reward/seahorse/q_six_final1/ \
   huggyllama/llama-7b \
   /data/pulkitag/models/idanshen/shared/models/sft/sft_seahorse/ \
   0.0067 \
