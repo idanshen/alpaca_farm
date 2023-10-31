f48837b954713237ad16b5f8df95336df6916027
Seungwook Han <swhan@mit.edu>
2023-10-31 14:35:23 -0400

    updated scripts for llama reward model ppo

--- scripts/run_ppo_seahorse.sh
+++ scripts/run_ppo_seahorse.sh
@@ -4,9 +4,10 @@ export WANDB_DATA_DIR=/data/pulkitag/models/idanshen/shared/wandb/
 export WANDB_CACHE_DIR=/data/pulkitag/models/idanshen/shared/wandb/wandb_cache/
 
 bash examples/scripts/rlhf_ppo.sh \
-  ./ppo_seahorse_flan_t5_kl0.0067/ \
-  run_ppo_seahorse_flan_t5 \
-  /data/pulkitag/models/idanshen/shared/models/reward/seahorse/q_six_final1/ \
+  ./ppo_seahorse_llama_kl0.0067/ \
+  run_ppo_seahorse_llama \
+  huggyllama/llama-7b \
+  /data/pulkitag/models/idanshen/shared/models/reward/seahorse/q_six_llama/ \
   huggyllama/llama-7b \
   /data/pulkitag/models/idanshen/shared/models/sft/sft_seahorse/ \
   0.0067 \
