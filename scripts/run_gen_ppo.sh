e86ad1cb86b14a9f538e46c2aa4f325f769ac54d
Seungwook Han <swhan@mit.edu>
2023-11-09 16:15:42 -0500

    updated path

--- scripts/run_gen_ppo.sh
+++ scripts/run_gen_ppo.sh
@@ -7,7 +7,7 @@ gpu=$3
 CUDA_VISIBLE_DEVICES=$gpu python examples/generate_qmodel.py \
   --decoder_name_or_path huggyllama/llama-7b \
   --decoder_checkpoint_dir $ppo_checkpoint \
-  --sft_checkpoint_dir /mnt/nfs_csail/models/idanshen/shared/models/sft/sft_seahorse/ \
+  --sft_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/sft_seahorse/ \
   --load_in_4_bits True \
   --temp 1.0 \
   --per_device_batch_size 1 \
