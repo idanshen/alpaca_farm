4273aba0eb6a3e3ecddb20931d8abbbfc5f7f702
Seungwook Han <swhan@mit.edu>
2023-11-09 16:25:27 -0500

    fixed split map for run_decode

--- examples/best_of_n.py
+++ examples/best_of_n.py
@@ -89,9 +89,10 @@ def run_decode(
         )
     elif 'seahorse' in dataset_path:
         dataset = datasets.load_dataset(dataset_path, data_files = {"train": "train.json", 'validation': 'validation.json'})
+        split_map = {"train": "train", "validation": "validation", "eval": "validation"}
         prompts, list_dict_data, metadata = data_preprocessor.format_prompt_with_dataset(
             dataset_path=dataset_path,
-            dataset=dataset[split],
+            dataset=dataset[split_map[split]],
             prompt_dict=utils.jload(prompt_dict_path),
         )
     else:
