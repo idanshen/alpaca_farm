23584d66a4348dbda79ef31e8179698a9c2409c7
Seungwook Han <swhan@mit.edu>
2023-11-10 14:45:18 -0500

    updated for seahorse

--- scripts/run_gen_q.sh
+++ scripts/run_gen_q.sh
@@ -6,18 +6,17 @@ gpu=$4
 
 CUDA_VISIBLE_DEVICES=$gpu python examples/generate_qmodel.py \
   --decoder_name_or_path huggyllama/llama-7b \
-  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/test_5/ \
+  --decoder_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/sft/sft_seahorse/ \
   --q_checkpoint_dir $q_checkpoint \
   --num_q_heads 1 \
   --q_head_type linear \
   --load_in_4_bits True \
-  --greedy True \
   --temp 1.0 \
   --per_device_batch_size 1 \
   --path_to_result $result_file \
   --beta $beta \
-  --dataset_path argilla/news-summary \
-  --dataset_name comparisons \
+  --dataset_path ./seahorse_data/ \
+  --dataset_name '' \
   --fp16 \
-  --flash_attn True
+  --flash_attn False
 
