7ada30c7adc3091bfa74c89672d5de26cbe3095f
Seungwook Han <swhan@mit.edu>
2023-11-10 11:19:08 -0500

    updated model checkpoint rm

--- scripts/run_eval_rm.sh
+++ scripts/run_eval_rm.sh
@@ -2,7 +2,7 @@
 
 CUDA_VISIBLE_DEVICES=1 python examples/evaluate_outputs_rm.py \
   --reward_model_name_or_path huggyllama/llama-7b \
-  --reward_model_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/reward/rlaif_openai_summarize_from_feedback5_1 \
+  --reward_model_checkpoint_dir /data/pulkitag/models/idanshen/shared/models/reward/rlaif_openai_summarize_from_feedback5/ \
   --output_filepath $1 \
   --path_to_result $2 \
   --per_device_batch_size 1 \
